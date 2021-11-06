526bdbd1f64c638ff1c9a4548339b07d8a9477af
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-11-06 18:05:49 -0300

    ultimo

--- newproduct.php
+++ newproduct.php
@@ -19,10 +19,11 @@
 		$activo = 1;
 	}
 	
-	//move_uploaded_file($_FILES['txtImagen1']['name'],"imagenes/".$id."_1.jpg");
-	//move_uploaded_file($_FILES['txtImagen2']['name'],"imagenes/".$id."_2.jpg");
-	//move_uploaded_file($_FILES['txtImagen3']['name'],"imagenes/".$id."_3.jpg");
-	$var = $_FILES['txtImagen1']['name'];
-	echo $var;
+	//move_uploaded_file($_FILES['img1']['tmp_name'],"imagenes/".$id."_1.jpg");
+	//move_uploaded_file($_FILES['img2']['tmp_name'],"imagenes/".$id."_2.jpg");
+	//move_uploaded_file($_FILES['img3']['tmp_name'],"imagenes/".$id."_3.jpg");
+	foreach ($_FILES["img"]['tmp_name'] as $key => $tmp_name){
+	echo $_FILES["img"]["name"][$key];
+	}
 //	guardar($id, $nom, $descripcion, $precio, $stock, $activo, "imagenes/".$id."_1.jpg", "imagenes/".$id."_2.jpg", "imagenes/".$id."_3.jpg");
 ?>
