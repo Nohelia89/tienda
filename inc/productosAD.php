85dda7f2454927e013133cfc8eaef4321456e666
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-11-06 18:33:04 -0300

    ultimo sabado

--- inc/productosAD.php
+++ inc/productosAD.php
@@ -1,18 +1,17 @@
 <?php
 	include("conex.php");
 		
-	function guardar($id, $nom, $descripcion, $precio, $stock, $activo, $img1, $img2, $img3){
+	function guardar($id, $nom, $descripcion, $precio, $stock, $activo, $imagenes){
 
 		$link = Conectarse();				
 		$sql = "insert into producto(id_producto, nombre, descripcion, precio, stock, activo) VALUES ($id,'$nom','$descripcion',$precio,$stock,$activo)";
 		mysqli_query($link, $sql);
 		
-		$sql = "insert into imagenes(id_producto, url_imagen) VALUES ($id,'$img1')";
-		mysqli_query($link, $sql);
-		$sql = "insert into imagenes(id_producto, url_imagen) VALUES ($id,'$img2')";
-		mysqli_query($link, $sql);
-		$sql = "insert into imagenes(id_producto, url_imagen) VALUES ($id,'$img3')";
-		mysqli_query($link, $sql);
+		for ($i=0; $i<count($imagenes); $i++){
+			$sql = "insert into imagenes(id_producto, url_imagen) VALUES ($id,'$imagenes[$i]')";
+			mysqli_query($link, $sql);
+		}
+
 		
 		mysqli_close($link);
 	}//end
--- newproduct.php
+++ newproduct.php
@@ -22,21 +22,20 @@
 	if (isset($_FILES['img'])){
 		
 		$cantidad= count($_FILES["img"]["tmp_name"]);
-		
+		$imagenes=array();
 		for ($i=0; $i<$cantidad; $i++){
 			//Comprobamos si el fichero es una imagen
 			if ($_FILES['img']['type'][$i]=='image/png' || $_FILES['img']['type'][$i]=='image/jpeg'){		
 				//Subimos el fichero al servidor
-				echo $_FILES["img"]["name"][$i];
-				//move_uploaded_file($_FILES["img"]["tmp_name"][$i], "imagenes/".$id."_".$i."jpg");
+			//	echo $_FILES["img"]["name"][$i];
+			$imagenes[$i]=$id."_".$i.".jpg";
+				move_uploaded_file($_FILES["img"]["tmp_name"][$i], "imagenes/".$id."_".$i.".jpg");
 				$validar=true;
 			}
 			else $validar=false;
 		}
 	}
-	//move_uploaded_file($_FILES['img1']['tmp_name'],"imagenes/".$id."_1.jpg");
-	//move_uploaded_file($_FILES['img2']['tmp_name'],"imagenes/".$id."_2.jpg");
-	//move_uploaded_file($_FILES['img3']['tmp_name'],"imagenes/".$id."_3.jpg");
+
 	
-//	guardar($id, $nom, $descripcion, $precio, $stock, $activo, "imagenes/".$id."_1.jpg", "imagenes/".$id."_2.jpg", "imagenes/".$id."_3.jpg");
+	guardar($id, $nom, $descripcion, $precio, $stock, $activo, $imagenes);
 ?>
