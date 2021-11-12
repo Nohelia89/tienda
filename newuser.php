af774e924c986ee4648b8c0721fa4526dd619103
Mauricio <mauriciopiccardo@gmail.com>
2021-11-12 16:19:12 -0300

    arreglo de codigo

--- newuser.php
+++ newuser.php
@@ -1,19 +1,18 @@
 <?php
+	include("inc/usuariosAD.php");
 
-include("inc/usuariosAD.php");
+	$id=$_POST['txtUser'];
+	$pass=$_POST['txtPass'];
+	$nom=$_POST['txtNom'];
+	$ape=$_POST['txtApe'];
+	$dir=$_POST['txtDir'];
+	$mail=$_POST['txtMail'];
 
-
-$id=$_POST['txtUser'];
-$pass=$_POST['txtPass'];
-$nom=$_POST['txtNom'];
-$ape=$_POST['txtApe'];
-$dir=$_POST['txtDir'];
-$mail=$_POST['txtMail'];
-if (!exists($id)){
-insertUser($id, $pass, $nom, $ape, $mail, $dir);
-header("Location:login.html");
-}
-else{
-    header("Location: pageerror.php?err=userex&pag=registro.html");
-}
+	if (!exists($id)){
+		insertUser($id, $pass, $nom, $ape, $mail, $dir);
+		header("Location:login.html");
+	}
+	else{
+		header("Location: pageerror.php?err=userex&pag=registro.html");
+	}
 ?>
